7734ea42ee3bf2991824fc6451343516c0493324
Naftuli Kay <me@naftuli.wtf>
2023-10-15 17:25:35 -0700

    full parsing using nom

--- example_parsing/src/parsing/semver.rs
+++ example_parsing/src/parsing/semver.rs
@@ -1,8 +1,17 @@
 #[cfg(test)]
 mod tests;
 
-use std::fmt::{Display, Formatter};
+use nom::character::{complete::char, complete::digit1};
+use nom::combinator::{map, opt};
+use nom::IResult;
 
+use anyhow::{Context, Error, Result};
+use nom::sequence::preceded;
+use std::fmt::{Display, Formatter};
+use std::str::FromStr;
+
+/// Representation of a semantic version with an optional bugfix revision.
+#[derive(Debug, Eq, PartialEq)]
 pub struct SemanticVersion {
     pub major: u64,
     pub minor: u64,
@@ -10,6 +19,7 @@ pub struct SemanticVersion {
 }
 
 impl SemanticVersion {
+    /// Create a new, full semantic version.
     pub fn new(major: u64, minor: u64, bugfix: u64) -> Self {
         Self {
             major,
@@ -18,6 +28,7 @@ impl SemanticVersion {
         }
     }
 
+    /// Create an abridged (major/minor only) semantic version.
     pub fn abridged(major: u64, minor: u64) -> Self {
         Self {
             major,
@@ -26,8 +37,40 @@ impl SemanticVersion {
         }
     }
 
+    /// Format this [SemanticVersion] to a string with a `v` prefix.
     pub fn v(&self) -> String {
         format!("v{}", self.to_string())
+    }
+
+    /// Parse a [SemanticVersion] from a string.
+    pub fn parse<S: AsRef<str>>(s: S) -> Result<Self> {
+        Self::from_str(s.as_ref())
+    }
+
+    /// Parse the input using [nom], returning a [IResult].
+    fn nom_parse(input: &str) -> IResult<&str, Self> {
+        // parse an optional 'v' prefix
+        let (input, _) = opt(char('v'))(input)?;
+        // as long as characters are base-10 digits, collect them and parse into a u64
+        let (input, major) = map(digit1, |s: &str| s.parse::<u64>().unwrap())(input)?;
+        // parse a '.'
+        let (input, _) = char('.')(input)?;
+        // as long as characters are base-10 digits, collect them and parse into a u64
+        let (input, minor) = map(digit1, |s: &str| s.parse::<u64>().unwrap())(input)?;
+        // optionally parse a '.' followed by base-10 digits, collect those into an Option<u64>
+        let (input, bugfix) = opt(preceded(
+            char('.'),
+            map(digit1, |s: &str| s.parse::<u64>().unwrap()),
+        ))(input)?;
+
+        Ok((
+            input,
+            Self {
+                major,
+                minor,
+                bugfix,
+            },
+        ))
     }
 }
 
@@ -41,4 +84,13 @@ impl Display for SemanticVersion {
     }
 }
 
-pub fn parse_major_minor(_input: &str) {}
+impl FromStr for SemanticVersion {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(SemanticVersion::nom_parse(s)
+            .map(|(_, v)| v)
+            .map_err(|e| e.to_owned())
+            .with_context(|| format!("Unable to parse input as semantic version"))?)
+    }
+}
--- example_parsing/src/parsing/semver/tests.rs
+++ example_parsing/src/parsing/semver/tests.rs
@@ -1,9 +1,58 @@
 use super::SemanticVersion;
 
+use std::str::FromStr;
+
 #[test]
-fn test_semver_display() {
+fn test_u64_parse_strip_leading_zeroes() {
+    assert_eq!(123u64, "000123".parse().unwrap());
+}
+#[test]
+fn test_display() {
+    // abridged, no prefix
     assert_eq!("5.6", SemanticVersion::abridged(5, 6).to_string());
+    // full, no prefix
     assert_eq!("1.2.3", SemanticVersion::new(1, 2, 3).to_string());
+    // full, v prefix
     assert_eq!("v0.0.1", SemanticVersion::new(0, 0, 1).v());
+    // abridged, v prefix
     assert_eq!("v0.1", SemanticVersion::abridged(0, 1).v());
+}
+
+#[test]
+fn test_nom_parse() {
+    // test full, no prefix
+    let (_, v) = SemanticVersion::nom_parse("1.2.3").expect("unable to parse 1.2.3");
+    assert_eq!(SemanticVersion::new(1, 2, 3), v);
+    // test full, prefix
+    let (_, v) = SemanticVersion::nom_parse("v5.6.7").expect("unable to parse v5.6.7");
+    assert_eq!(SemanticVersion::new(5, 6, 7), v);
+    // test abridged, no prefix
+    let (_, v) = SemanticVersion::nom_parse("0.1").expect("unable to parse 0.1");
+    assert_eq!(SemanticVersion::abridged(0, 1), v);
+    // test abridged, prefix
+    let (_, v) = SemanticVersion::nom_parse("v2.3").expect("unable to parse v2.3");
+    assert_eq!(SemanticVersion::abridged(2, 3), v);
+    // test failures
+    assert!(SemanticVersion::nom_parse("unrelated").is_err());
+    // FIXME do we care enough to make the parser complete, not allowing trailing data?
+}
+
+#[test]
+fn test_from_str() {
+    assert_eq!(
+        SemanticVersion::new(1, 2, 3),
+        SemanticVersion::from_str("1.2.3").expect("unable to parse 1.2.3")
+    );
+    assert_eq!(
+        SemanticVersion::new(5, 6, 7),
+        SemanticVersion::from_str("v5.6.7").expect("unable to parse v5.6.7")
+    );
+    assert_eq!(
+        SemanticVersion::abridged(7, 8),
+        SemanticVersion::from_str("7.8").expect("unable to parse 7.8")
+    );
+    assert_eq!(
+        SemanticVersion::abridged(0, 1),
+        SemanticVersion::from_str("v0.1").expect("unable to parse v0.1")
+    );
 }
